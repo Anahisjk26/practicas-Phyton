43543f63fb2c5abc5f054adc843d595e00a0e356
Anahi Sajama <lauraanahisajama@gmail.com>
2025-11-02 09:32:11 -0300

    mejora del juego

--- juego_adivinanza.py
+++ juego_adivinanza.py
@@ -8,22 +8,29 @@ def jugar_adivinanza():
 
     # Seleccionar una palabra secreta aleatoriamente
     palabra_secreta = random.choice(palabras)
-    
-    print("Ha sido seleccionada una palabra secreta. Tienes 3 intentos para adivinarla.")
-    
-    for i in range(len(palabra_secreta)):
-        print("_", end=" ")
-    
     intentos = 3
 
+    print("Ha sido seleccionada una palabra secreta. Tienes 3 intentos para adivinarla.")
+    for i in range(len(palabra_secreta)):
+        if i == 0:
+            print(f"{palabra_secreta[i]} ", end="")
+        print("_ ", end="")
+    print()
+
     while intentos > 0:
-        intento = input("Introduce tu intento: ")
-        if intento.lower() == palabra_secreta:
+        intento = input("Introduce tu intento: ").lower()
+        if intento == palabra_secreta:
             print("¡Felicidades! Has adivinado la palabra secreta.")
             break
         else:
             intentos -= 1
-            print(f"Intenta de nuevo. Te quedan {intentos} intentos.")
+            print(f"Incorrecto. Te quedan {intentos} intentos.")
+        if intentos == 2:
+            pista = palabra_secreta[1]  # Primera letra como pista
+            print(f"Pista: La primera letra de la palabra es '{pista}'.")
+        else:
+            pista = palabra_secreta[1:3]  # Primeras dos letras como pista
+            print(f"Pista: Las primeras letras de la palabra son '{pista}'.")
 
     if intentos == 0:
         print(f"Lo siento, has perdido. La palabra secreta era '{palabra_secreta}'.")
